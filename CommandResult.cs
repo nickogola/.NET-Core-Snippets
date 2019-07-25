719af642ca4560e7375101ab90df5efd1d6fc246
nickogola <ogolanick@gmail.com>
2019-07-25 02:46:11 -0400

    Add files via upload

---
+++ CommandResult.cs
@@ -0,0 +1,41 @@
+﻿namespace [custom]
+{
+    public class CommandResult
+    {
+        private CommandResult() { }
+
+        private CommandResult(object data)
+        {
+            this.Data = data;
+        }
+
+        private CommandResult(string failureReason)
+        {
+            this.FailureReason = failureReason;
+        }
+
+        public object Data { get; }
+        public string FailureReason { get; }
+        public bool IsSuccess => string.IsNullOrEmpty(FailureReason);
+
+        public static CommandResult Success()
+        {
+            return new CommandResult();
+        }
+
+        public static CommandResult Success(object data)
+        {
+            return new CommandResult(data);
+        }
+
+        public static CommandResult Fail(string reason)
+        {
+            return new CommandResult(reason);
+        }
+
+        public static implicit operator bool(CommandResult result)
+        {
+            return result.IsSuccess;
+        }
+    }
+}
