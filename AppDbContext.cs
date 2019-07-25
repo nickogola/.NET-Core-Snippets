a5adf03569c20d8f915d565d4886379606f76b59
nickogola <ogolanick@gmail.com>
2019-07-25 09:19:17 -0400

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,5 +1,8 @@
 ﻿namespace [custom]
 {
+    /*
+        Inherits from DbContextBase and IAppDbContext
+    */
     public class AppDbContext : DbContextBase, IAppDbContext
     {
         public AppDbContext(string connectionString) : base(connectionString)
