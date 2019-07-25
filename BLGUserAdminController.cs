89a8794f03a099136eb69f554f4c31c042fe17a5
nickogola <ogolanick@gmail.com>
2019-07-25 02:50:54 -0400

    Add files via upload

---
+++ BLGUserAdminController.cs
@@ -0,0 +1,44 @@
+﻿using BLG.AspNetCore;
+using BLG.AspNetCore.Filters;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLG.IT.Web.Features.BLGUserAdmin
+{
+    [Authorize(Policy = Constants.BLGITBLGUserAdminSecurityPolicy)]
+    public class BLGUserAdminController : Controller
+    {
+        private readonly IMediator _mediator; // MediatR library routes data from this controller to AddUser.cs
+
+        public BLGUserAdminController(IMediator mediator)
+        {
+            _mediator = mediator; //mediatr injected
+        }
+
+    
+        public async Task<IActionResult> Add(Add.Query query)
+        {
+            var model = await _mediator.Send(query);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Add.Command command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result)
+                return this.CommandResultFailureJson(result);
+
+            return this.RedirectToActionJson(nameof(Index));
+        }
+
+
+    }
+}
